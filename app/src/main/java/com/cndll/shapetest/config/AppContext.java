0b04680065034ad2d0fb83d9d5df4d7e1378c5f8
xzy <xzy@qq.com>
2017-06-27 08:43:45 +0800

    全局变量

--- app/src/main/java/com/cndll/shapetest/config/AppContext.java
+++ app/src/main/java/com/cndll/shapetest/config/AppContext.java
@@ -28,24 +28,18 @@ public class AppContext extends Application implements UncaughtExceptionHandler 
     public void onCreate() {
         super.onCreate();
         appContext = this;
-//        MultiDex.install(this);
         mContext = this.getApplicationContext();
-//        Fresco.initialize(this);
-//        EaseUI.getInstance().init(this, null);
-//        EMClient.getInstance().setDebugMode(false);
         // 获取系统默认的UncaughtException处理
         mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
         //友盟配置三方平台的appkey
         PlatformConfig.setWeixin("wxd96b8dd3e5733967", "de1d138b686684274a39964f931b7eea");
         PlatformConfig.setQQZone("1105155596", "bbRRZKif6xhk679w");
         PlatformConfig.setSinaWeibo("3588954476", "66ee7a15a0301492373f82a42a80cd62", "http://www.sina.com");
-        //Config.DEBUG = true;
         Config.isJumptoAppStore = true;
         UMShareAPI.get(this);
         msgApi = WXAPIFactory.createWXAPI(appContext, null);
         // 将该app注册到微信
         msgApi.registerApp("wxd96b8dd3e5733967");
-//        CrashReport.initCrashReport(getApplicationContext(), "23c6e1948f", false);
         mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
     }
 
