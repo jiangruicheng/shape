648a95d6e5200d9494d10a9b537dde9a2c260225
xzy <xzy@qq.com>
2017-08-04 11:16:17 +0800

    订单

--- app/src/main/java/com/cndll/shapetest/activity/LoginActivity.kt
+++ app/src/main/java/com/cndll/shapetest/activity/LoginActivity.kt
@@ -89,7 +89,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
                     binding.handler.login(binding.loginbtn)
                     finish()
                 }else{
-                    Toast.makeText(context,t.error_message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(context,t.message,Toast.LENGTH_LONG).show()
                     return
                 }
             }
